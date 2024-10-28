a86bc5ab50a99c5e5815b1c42f78cc3bc11c204d
Ed Gerald <edgerald.gonzales.eg@gmail.com>
2024-10-28 10:08:49 +0800

    fix front-end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,6 @@ import App from "./App.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
-import customFetch from "./utils/customFetch.jsx";
-import axios from "axios";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
